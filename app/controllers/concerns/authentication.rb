4666c1a855ae9815815c0ce356570a59326b63a5
Keenan Brock <keenan@thebrocks.net>
2015-10-12 00:07:04 -0400

    authentication (step 1)

---
+++ app/controllers/concerns/authentication.rb
@@ -0,0 +1,26 @@
+module Authentication
+  def username
+    cookies.signed[:user_id]
+  end
+
+  def username=(userid)
+    cookies.signed[:user_id] = userid
+  end
+
+  def mode=(mode)
+    cookies.signed[:mode] = mode.to_s
+  end
+
+  def edit_mode?
+    cookies.signed[:mode] == "edit"
+  end
+
+  def can?(action = :view, object = nil)
+    action == :create ? edit_mode? : true
+  end
+
+  def self.included(base)
+    base.helper_method :username
+    base.helper_method :can?
+  end
+end
